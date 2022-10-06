01917961484185c84da3631119891da6d5b7c1ea
Manav Misra <manavm@visionwebsoft.com>
2022-10-05 19:57:48 -0500

    Add some silly solutions for form class toggles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-// TODO: Log the input from the form
-
 const form = document.querySelector("form");
 
-const button = document.querySelector("button");
+const inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   console.log(event.target.elements[0].value);
   console.log(event.target.elements[1].value);
+});
+
+inputs[0].addEventListener("blur", (event) => {
+  const input = event.target;
+
+  if (!input.value.includes("@")) {
+    input.classList.add("border-rose-500");
+  }
+});
+
+inputs[0].addEventListener("focus", (event) => {
+  const input = event.target;
+
+  input.classList.remove("border-rose-500");
+});
+
+inputs[1].addEventListener("blur", (event) => {
+  const input = event.target;
+
+  if (input.value.length < 6) {
+    input.classList.add("border-rose-500");
+  }
 });
